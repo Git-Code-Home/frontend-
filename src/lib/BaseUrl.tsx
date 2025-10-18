2e8e5d00850a1b81c4d0ca5e745b656efc0020c2
Git-Code-Home <kashafsarfraz12@gmail.com>
2025-10-18 22:12:11 +0500

    BaseUrl change

--- src/lib/BaseUrl.tsx
+++ src/lib/BaseUrl.tsx
@@ -2,5 +2,5 @@
 // const BASE_URL = "http://visa-management-backend.vercel.app/api"; 
 
 // export default BASE_URL;
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "http://sherry-backend.vercel.app";
 export default BASE_URL;
